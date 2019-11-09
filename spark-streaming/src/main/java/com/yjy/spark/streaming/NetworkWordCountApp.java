8a8afb0589c4c7e4e6ff8ae84416d5edf0d3513e
yanjingyang <yanjingyang>
2019-11-09 19:46:19 +0800

    WordCount

---
+++ spark-streaming/src/main/java/com/yjy/spark/streaming/NetworkWordCountApp.java
@@ -0,0 +1,36 @@
+package com.yjy.spark.streaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class NetworkWordCountApp {
+
+    public static void main(String[] args) throws InterruptedException {
+        SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCountApp");
+
+        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(1));
+
+        JavaReceiverInputDStream<String> lines = streamingContext.socketTextStream("vhost1", 9999);
+
+        JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+
+        JavaPairDStream<String, Integer> pairs = words.mapToPair((PairFunction<String, String, Integer>) word -> new Tuple2<String, Integer>(word, 1));
+
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(Integer::sum);
+
+        wordCounts.print();
+
+        streamingContext.start();
+
+        streamingContext.awaitTermination();
+    }
+
+}
--- spark-streaming/src/main/java/com/yjy/spark/streaming/StatefulWordCountApp.java
+++ spark-streaming/src/main/java/com/yjy/spark/streaming/StatefulWordCountApp.java
@@ -3,6 +3,7 @@ package com.yjy.spark.streaming;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.Optional;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
@@ -13,10 +14,10 @@ import scala.Tuple2;
 import java.util.Arrays;
 import java.util.List;
 
-public class StatefulWordCount {
+public class StatefulWordCountApp {
 
     public static void main(String[] args) throws InterruptedException {
-        SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("StatefulWordCount");
+        SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("StatefulWordCountApp");
 
         JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(5));
         // 如果使用stateful的算子，必须要设置checkpoint
@@ -26,7 +27,7 @@ public class StatefulWordCount {
 
         JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
-        JavaPairDStream<String, Integer> pairs = words.mapToPair(word -> new Tuple2<>(word, 1));
+        JavaPairDStream<String, Integer> pairs = words.mapToPair((PairFunction<String, String, Integer>) word -> new Tuple2<String, Integer>(word, 1));
 
         Function2<List<Integer>, Optional<Integer>, Optional<Integer>> updateFunction =
                 (values, state) -> {
